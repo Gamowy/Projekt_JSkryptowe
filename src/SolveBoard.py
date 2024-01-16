f89cac9f3975b3f507b2db8d9193b0921c5aed6f
Patryk Gamrat <Patryk.gam@gmail.com>
2024-01-16 18:16:09 +0100

    Add file handling

--- src/SolveBoard.py
+++ src/SolveBoard.py
@@ -1,15 +1,24 @@
+import sys
+
+
 def loadBoard(filename: str) -> list:
     loaded_board = []
     try:
-        with open(filename) as file:
-            lines = file.readlines()
+        with open(filename, encoding='utf=8') as board_file:
+            lines = board_file.readlines()
             lines = [l.strip() for l in lines]
             for line in lines:
                 loaded_board.append(line.split(' '))
+
+        # Sprawdzanie poprawności wczytania danych
+        if len(loaded_board) != 5:
+            raise IOError
+        for l in range(5):
+            if len(loaded_board[l]) != 5 or not all(s == 'x' or s == 'o' for s in loaded_board[l]):
+                raise IOError
+        return loaded_board
     except IOError:
-        print("Nie udało się załadować pliku game.txt z planszą!")
-        return None
-    return loaded_board
+        print(f"Nie udało się załadować pliku {filename} z planszą!")
 
 
 def addPoints(count: int, points: list):
@@ -29,67 +38,78 @@ def addPoints(count: int, points: list):
 
 
 if __name__ == "__main__":
-    board = loadBoard('game.txt')
-    usedPatterns = [[[False for _ in range(4)] for _ in range(5)] for _ in range(5)]
-    kacperPoints = [0 for _ in range(4)]
-    olekPoints = [0 for _ in range(4)]
+    board = None
+    try:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+    except IndexError:
+        raise SystemExit("Nie podano scieżki do plików!")
 
+    board = loadBoard(input_file)
     if board is not None:
+        used_patterns = [[[False for _ in range(4)] for _ in range(5)] for _ in range(5)]
+        kacper_points = [0 for _ in range(4)]
+        olek_points = [0 for _ in range(4)]
         for i in range(5):
             for j in range(5):
                 symbol = board[i][j]
 
                 # Wiersze
                 n = 0
-                while (j + n < 5) and (board[i][j + n] == symbol) and (usedPatterns[i][j + n][0] == False):
-                    usedPatterns[i][j + n][0] = True
+                while (j + n < 5) and (board[i][j + n] == symbol) and (used_patterns[i][j + n][0] == False):
+                    used_patterns[i][j + n][0] = True
                     n += 1
                 if symbol == 'x':
-                    addPoints(n, kacperPoints)
+                    addPoints(n, kacper_points)
                 else:
-                    addPoints(n, olekPoints)
+                    addPoints(n, olek_points)
 
                 # Kolumny
                 n = 0
-                while (i + n < 5) and (board[i + n][j] == symbol) and (usedPatterns[i + n][j][1] == False):
-                    usedPatterns[i+n][j][1] = True
+                while (i + n < 5) and (board[i + n][j] == symbol) and (used_patterns[i + n][j][1] == False):
+                    used_patterns[i+n][j][1] = True
                     n += 1
                 if symbol == 'x':
-                    addPoints(n, kacperPoints)
+                    addPoints(n, kacper_points)
                 else:
-                    addPoints(n, olekPoints)
+                    addPoints(n, olek_points)
 
                 # Diagonale lewe
                 n = 0
-                while (i + n < 5) and (j - n >= 0) and (board[i + n][j - n] == symbol) and (usedPatterns[i + n][j - n][2] == False):
-                    usedPatterns[i+n][j-n][2] = True
+                while (i + n < 5) and (j - n >= 0) and (board[i + n][j - n] == symbol) and (used_patterns[i + n][j - n][2] == False):
+                    used_patterns[i+n][j-n][2] = True
                     n += 1
                 if symbol == 'x':
-                    addPoints(n, kacperPoints)
+                    addPoints(n, kacper_points)
                 else:
-                    addPoints(n, olekPoints)
+                    addPoints(n, olek_points)
 
                 # Diagonale prawe
                 n = 0
-                while (i + n < 5) and (j + n < 5) and (board[i + n][j + n] == symbol) and (usedPatterns[i + n][j + n][3] == False):
-                    usedPatterns[i+n][j+n][3] = True
+                while (i + n < 5) and (j + n < 5) and (board[i + n][j + n] == symbol) and (used_patterns[i + n][j + n][3] == False):
+                    used_patterns[i+n][j+n][3] = True
                     n += 1
                 if symbol == 'x':
-                    addPoints(n, kacperPoints)
+                    addPoints(n, kacper_points)
                 else:
-                    addPoints(n, olekPoints)
+                    addPoints(n, olek_points)
 
         # Sumowanie punktów
-        sumKacper = kacperPoints[3] * 15 + kacperPoints[2] * 7 + kacperPoints[1] * 3 + kacperPoints[0] * 1
-        sumOlek = olekPoints[3] * 15 + olekPoints[2] * 7 + olekPoints[1] * 3 + olekPoints[0] * 1
+        sumKacper = kacper_points[3] * 15 + kacper_points[2] * 7 + kacper_points[1] * 3 + kacper_points[0] * 1
+        sumOlek = olek_points[3] * 15 + olek_points[2] * 7 + olek_points[1] * 3 + olek_points[0] * 1
 
         # Wypisanie wyników
-        print(f"Punkty dla gracza umieszczającego \"x\": {kacperPoints[3]}*15 + {kacperPoints[2]}*7 + {kacperPoints[1]}*3 + {kacperPoints[0]}*1 = {sumKacper}")
-        print(f"Punkty dla gracza umieszczającego \"o\": {olekPoints[3]}*15 + {olekPoints[2]}*7 + {olekPoints[1]}*3 + {olekPoints[0]}*1 = {sumOlek}")
-
-        if sumKacper > sumOlek:
-            print("Wygrał kacper")
-        elif sumOlek > sumKacper:
-            print("Wygrał Olek")
-        else:
-            print("Jest remis")
+        try:
+            with open(output_file, "w", encoding="utf-8") as out_file:
+                for row in board:
+                    print(*row, file=out_file)
+                print(f"Punkty dla gracza umieszczającego \"x\": {kacper_points[3]}*15 + {kacper_points[2]}*7 + {kacper_points[1]}*3 + {kacper_points[0]}*1 = {sumKacper}", file=out_file)
+                print(f"Punkty dla gracza umieszczającego \"o\": {olek_points[3]}*15 + {olek_points[2]}*7 + {olek_points[1]}*3 + {olek_points[0]}*1 = {sumOlek}", file=out_file)
+                if sumKacper > sumOlek:
+                    print("Wygrał kacper", file=out_file)
+                elif sumOlek > sumKacper:
+                    print("Wygrał Olek", file=out_file)
+                else:
+                    print("Jest remis", file=out_file)
+        except IOError:
+            print(f"Nie udało się zapisać do pliku {output_file}!")
